ea4789c9bdf40d29503fdbc20a1d42d57e08da53
Luis Ferreira <luisfferreira@outlook.com>
2014-10-06 16:40:42 +0200

    size matcher now only asserts something when it has a valid result

--- perception/size_matcher.cpp
+++ perception/size_matcher.cpp
@@ -64,11 +64,13 @@ void SizeMatcher::process(ed::EntityConstPtr e, tue::Configuration& result) cons
 
     const cv::Mat& rgb_image = msr->image()->getRGBImage();
 
+    // initialize min and max coordinates
     geo::Vector3 min(1e10, 1e10, 1e10);
     geo::Vector3 max(-1e10, -1e10, -1e10);
 
     rgbd::View view(*msr->image(), msr->imageMask().width());
 
+    // get minimun and maximum X, Y and Z coordinates of the object
     for(ed::ImageMask::const_iterator it = msr->imageMask().begin(view.getWidth()); it != msr->imageMask().end(); ++it)
     {
         const cv::Point2i& p_2d = *it;
@@ -77,25 +79,27 @@ void SizeMatcher::process(ed::EntityConstPtr e, tue::Configuration& result) cons
         if (view.getPoint3D(p_2d.x, p_2d.y, p))
         {
             geo::Vector3 p_MAP = msr->sensorPose() * p;
+//            geo::Vector3 p_MAP = p;     // use this only when testing
+
             min.x = std::min(min.x, p_MAP.x); max.x = std::max(max.x, p_MAP.x);
             min.y = std::min(min.y, p_MAP.y); max.y = std::max(max.y, p_MAP.y);
             min.z = std::min(min.z, p_MAP.z); max.z = std::max(max.z, p_MAP.z);
         }
     }
 
+    // determine size
     geo::Vector3 size = max - min;
-    double width = sqrt(size.x * size.x + size.z * size.z);
-    double height = size.y;
+    double object_width = sqrt(size.x * size.x + size.z * size.z);
+    double object_height = size.y;
 
     result.writeGroup("size");
-    result.setValue("width", width);
-    result.setValue("height", height);
+    result.setValue("width", object_width);
+    result.setValue("height", object_height);
     result.endGroup();
-
-//    std::cout << "SizeMatcher: width = " << width << ", height = " << height << std::endl << std::endl;
 
     std::vector<std::string> hyps;
 
+    // compare object size to loaded models
     for(std::map<std::string, std::vector<ObjectSize> >::const_iterator it = models_.begin(); it != models_.end(); ++it)
     {
         const std::string& label = it->first;
@@ -104,10 +108,11 @@ void SizeMatcher::process(ed::EntityConstPtr e, tue::Configuration& result) cons
         bool match = false;
         for(std::vector<ObjectSize>::const_iterator it_size = sizes.begin(); it_size != sizes.end(); ++it_size)
         {
-            const ObjectSize& size = *it_size;
-//            std::cout << "    " << size.min_width << " " << size.max_width << std::endl;
-            if (height >= size.min_height && height <= size.max_height &&
-                    width >= size.min_width && width <= size.max_width)
+            const ObjectSize& model_size = *it_size;
+
+            // if the object is smaller or the same size as the model, add hypothesis
+            if (object_height >= model_size.min_height && object_height <= model_size.max_height &&
+                    object_width >= model_size.min_width && object_width <= model_size.max_width)
             {
                 match = true;
                 break;
@@ -116,23 +121,15 @@ void SizeMatcher::process(ed::EntityConstPtr e, tue::Configuration& result) cons
 
         if (match)
             hyps.push_back(label);
-        else
-        {
-            // Set this hypothesis to 0
-            result.writeArray("hypotheses");
-            result.addArrayItem();
-            result.setValue("type", label);
-            result.setValue("score", 0);
-            result.endArrayItem();
-            result.endArray(); // end "hypotheses"
-        }
     }
 
+    // if an hypothesis is found, assert it
     if (!hyps.empty())
     {
-        result.writeArray("hypotheses");
-
+        // probability depends on the number of hypothesis
         double prob = 1.0 / hyps.size();
+
+        result.writeArray("hypothesis");
         for(std::vector<std::string>::const_iterator it = hyps.begin(); it != hyps.end(); ++it)
         {
             result.addArrayItem();
@@ -140,8 +137,7 @@ void SizeMatcher::process(ed::EntityConstPtr e, tue::Configuration& result) cons
             result.setValue("score", prob);
             result.endArrayItem();
         }
-
-        result.endArray(); // end "hypotheses"
+        result.endArray();
     }
 }
 
