fa2a71295a71b749eb185882890cc6a6243e48fc
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-11-19 13:49:53 +0100

    RobotPlugin creates transform graph from URDF model

--- plugins/robot_plugin.cpp
+++ plugins/robot_plugin.cpp
@@ -1,6 +1,9 @@
 #include "robot_plugin.h"
 
+#include <kdl_parser/kdl_parser.hpp>
+
 #include <ros/node_handle.h>
+
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -12,6 +15,69 @@ RobotPlugin::RobotPlugin()
 
 RobotPlugin::~RobotPlugin()
 {
+    for(std::map<std::string, Joint*>::iterator it = joints_.begin(); it != joints_.end(); ++it)
+        delete it->second;
+
+    for(std::map<std::string, Link*>::iterator it = links_.begin(); it != links_.end(); ++it)
+        delete it->second;
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+void RobotPlugin::constructRobot(unsigned int parent_idx, const KDL::SegmentMap::const_iterator& it_segment)
+{
+    const KDL::Segment& segment = it_segment->second.segment;
+
+    // Create link
+    Link* link(new Link);
+    link->name = segment.getName();
+
+    // Add link
+    unsigned int child_idx = addLink(link);
+
+    // Create joint
+    Joint* joint(new Joint);
+    joint->position = 0;
+    joint->segment = segment;
+    joint->parent_idx = parent_idx;
+    joint->child_idx = child_idx;
+    joint->name = segment.getJoint().getName();
+
+    // Add joint
+    addJoint(joint);
+
+    // Calculate pose with default joint position (0)
+    KDL::Frame pose_kdl = segment.pose(joint->position);
+    joint->transform.R = geo::Matrix3(pose_kdl.M.data);
+    joint->transform.t = geo::Vector3(pose_kdl.p.data);
+
+    // Recursively add all children
+    const std::vector<KDL::SegmentMap::const_iterator>& children = it_segment->second.children;
+    for (unsigned int i = 0; i < children.size(); i++)
+        constructRobot(child_idx, children[i]);
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+unsigned int RobotPlugin::addLink(Link* link)
+{
+    unsigned int idx = nodes_.size();
+    nodes_.push_back(link);
+    links_[link->name] = link;
+    return idx;
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+unsigned int RobotPlugin::addJoint(Joint* joint)
+{
+    joints_[joint->name] = joint;
+
+    unsigned int idx = joint->parent_idx;
+    if (idx >= edges_.size())
+        edges_.resize(idx + 1);
+
+    edges_[idx].push_back(joint);
 }
 
 // ----------------------------------------------------------------------------------------------------
@@ -53,6 +119,28 @@ void RobotPlugin::configure(tue::Configuration config)
     if (config.hasError())
         return;
 
+    std::string urdf_xml;
+    if (!nh.getParam(urdf_rosparam, urdf_xml))
+    {
+        config.addError("No such ROS parameter: '" + urdf_rosparam + "'.");
+        return;
+    }
+
+    KDL::Tree tree;
+    if (!kdl_parser::treeFromString(urdf_xml, tree))
+    {
+        config.addError("Could not initialize KDL tree object.");
+        return;
+    }
+
+    std::cout << "OK" << std::endl;
+
+    // Add root link
+    root_ = new Link;
+    root_->name = "robot";
+    unsigned int root_idx = addLink(root_);
+
+    constructRobot(root_idx, tree.getRootSegment());
 }
 
 // ----------------------------------------------------------------------------------------------------
--- plugins/robot_plugin.h
+++ plugins/robot_plugin.h
@@ -7,6 +7,24 @@
 #include <ros/callback_queue.h>
 #include <sensor_msgs/JointState.h>
 
+#include <kdl/tree.hpp>
+#include <geolib/datatypes.h>
+
+struct Joint
+{
+    std::string name;
+    unsigned int parent_idx;
+    unsigned int child_idx;
+    double position;
+    KDL::Segment segment; // calculates the joint pose
+    geo::Pose3D transform;
+};
+
+struct Link
+{
+    std::string name;
+};
+
 class RobotPlugin : public ed::Plugin
 {
 
@@ -24,11 +42,36 @@ public:
 
 private:
 
+    // Joint and link info
+
+    std::map<std::string, Joint*> joints_;
+
+    std::map<std::string, Link*> links_;
+
+
+    // Transform graph
+
+    Link* root_;
+
+    std::vector<Link*> nodes_;
+
+    std::vector<std::vector<Joint*> > edges_;
+
+    void constructRobot(unsigned int parent_idx, const KDL::SegmentMap::const_iterator& it_segment);
+
+    unsigned int addLink(Link* link);
+
+    unsigned int addJoint(Joint* joint);
+
+
+    // ROS Communication
+
     ros::CallbackQueue cb_queue_;
 
     std::map<std::string, ros::Subscriber> joint_subscribers_;
 
     void jointCallback(const sensor_msgs::JointState::ConstPtr& msg);
+
 
 };
 
