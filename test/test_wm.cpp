a33045edd7df0ca41124e14c6d975cff8cf9f465
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-12-02 09:15:10 +0100

    More difficult graph search test

--- test/test_wm.cpp
+++ test/test_wm.cpp
@@ -25,6 +25,20 @@ void buildWorldModel(ed::WorldModel& wm)
     t2->insert(0, geo::Pose3D(1, 0, 0.75).inverse());
     req.setRelation("bottle", "table", t2);
 
+    std::string parent_id = "bottle";
+    for(unsigned int i = 0; i < 100000; ++i)
+    {
+        std::stringstream id;
+        id << "e" << i;
+        req.setType(id.str(), "object");
+
+        boost::shared_ptr<ed::TransformCache> t1(new ed::TransformCache());
+        t1->insert(0, geo::Pose3D(1, 2, 3, 0, 0, M_PI / 2));
+        req.setRelation(parent_id, id.str(), t1);
+
+        parent_id = id.str();
+    }
+
     wm.update(req);
 }
 
@@ -32,10 +46,10 @@ void buildWorldModel(ed::WorldModel& wm)
 
 void profile(const ed::WorldModel& wm)
 {
-    unsigned int N = 1000000;
+    unsigned int N = 1000;
 
     ed::UUID id1 = "map";
-    ed::UUID id2 = "bottle";
+    ed::UUID id2 = "e100";
 
     tue::Timer timer;
     timer.start();
