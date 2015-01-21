71e85c97596eb894c0cae21ef1c94393d5593c4e
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-01-22 00:29:09 +0100

    Added cache for model loading; is much faster now (from 1d8973e)

---
+++ include/ed/models/model_loader.h
@@ -0,0 +1,47 @@
+#ifndef ED_MODEL_LOADER_H_
+#define ED_MODEL_LOADER_H_
+
+#include "ed/uuid.h"
+
+#include <map>
+#include <geolib/datatypes.h>
+#include <tue/config/data_pointer.h>
+
+namespace ed
+{
+
+namespace models
+{
+
+class ModelLoader
+{
+
+public:
+
+    ModelLoader();
+
+    ~ModelLoader();
+
+    bool create(const UUID& id, const std::string& type, UpdateRequest& req);
+
+    bool create(const tue::config::DataConstPointer& data, UpdateRequest& req);
+
+    bool create(const tue::config::DataConstPointer& data, const UUID& id_opt, const UUID& parent_id, UpdateRequest& req, const std::string& model_path = "");
+
+private:
+
+    // Model name to model data
+    std::map<std::string, tue::config::DataConstPointer> model_cache_;
+
+    // Shape filename to shape
+    std::map<std::string, geo::ShapePtr> shape_cache_;
+
+    tue::config::DataConstPointer loadModelData(const std::string& type, std::string& model_path_str);
+
+};
+
+} // end namespace models
+
+} // end namespace ed
+
+#endif
