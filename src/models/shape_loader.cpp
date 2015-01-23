658386e745041d66b2386597aef10ff30eae30a2
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-01-23 14:54:22 +0100

    Height map meshing: can deal with holes

--- src/models/shape_loader.cpp
+++ src/models/shape_loader.cpp
@@ -28,14 +28,15 @@ namespace models
 
 // ----------------------------------------------------------------------------------------------------
 
-void findContours(const cv::Mat& image, const geo::Vec2i& p, std::vector<geo::Vec2i>& points)
+void findContours(const cv::Mat& image, const geo::Vec2i& p, int d_start, std::vector<geo::Vec2i>& points,
+                  std::vector<geo::Vec2i>& line_starts, cv::Mat& contour_map, bool add_first)
 {
     static int dx[4] = {1,  0, -1,  0 };
     static int dy[4] = {0,  1,  0, -1 };
 
     unsigned char v = image.at<unsigned char>(p.y, p.x);
 
-    int d_current = 0; // Current direction
+    int d_current = d_start; // Current direction
     int x2 = p.x;
     int y2 = p.y;
 
@@ -46,7 +47,8 @@ void findContours(const cv::Mat& image, const geo::Vec2i& p, std::vector<geo::Ve
                             // that gradually changes to the side (1-cell side steps), this direction
                             // denotes the principle axis of the line
 
-    points.push_back(p - geo::Vec2i(1, 1));
+    if (add_first)
+        points.push_back(p - geo::Vec2i(1, 1));
 
     int n_uninterrupted = 1;
     geo::Vec2i p_corner = p;
@@ -146,6 +148,11 @@ void findContours(const cv::Mat& image, const geo::Vec2i& p, std::vector<geo::Ve
             n_uninterrupted = 0;
         }
 
+        if ((d_current == 3 && d != 2) || (d == 3 && d != 0)) // up
+            line_starts.push_back(p_current);
+
+        contour_map.at<unsigned char>(p_current.y, p_current.x) = 1;
+
         ++n_uninterrupted;
 
         if (points.size() > 1 && x2 == p.x && y2 == p.y)
@@ -182,6 +189,7 @@ geo::ShapePtr getHeightMapShape(const tue::filesystem::Path& path, tue::config::
     }
 
     cv::Mat vertex_index_map(image.rows, image.cols, CV_32SC1, -1);
+    cv::Mat contour_map(image.rows, image.cols, CV_8UC1, cv::Scalar(0));
 
     boost::shared_ptr<geo::CompositeShape> shape(new geo::CompositeShape);
 
@@ -193,20 +201,22 @@ geo::ShapePtr getHeightMapShape(const tue::filesystem::Path& path, tue::config::
 
             if (v < 255)
             {
-                std::vector<geo::Vec2i> points;
-                findContours(image, geo::Vec2i(x, y), points);
+                std::vector<geo::Vec2i> points, line_starts;
+                findContours(image, geo::Vec2i(x, y), 0, points, line_starts, contour_map, true);
 
                 int num_points = points.size();
 
                 if (num_points > 2)
                 {
-                    TPPLPoly poly;
-                    poly.Init(num_points);
-
                     geo::Mesh mesh;
 
                     double min_z = origin_z;
                     double max_z = origin_z + (double)(255 - v) / 255 * blockheight;
+
+                    std::list<TPPLPoly> testpolys;
+
+                    TPPLPoly poly;
+                    poly.Init(num_points);
 
                     for(unsigned int i = 0; i < num_points; ++i)
                     {
@@ -217,11 +227,11 @@ geo::ShapePtr getHeightMapShape(const tue::filesystem::Path& path, tue::config::
                         double wx = points[i].x * resolution + origin_x;
                         double wy = (image.rows - points[i].y - 1) * resolution + origin_y;
 
-                        vertex_index_map.at<int>(points[i].y, points[i].x) = 2 * i;
-
-                        mesh.addPoint(geo::Vector3(wx, wy, min_z));
+                        vertex_index_map.at<int>(points[i].y, points[i].x) = mesh.addPoint(geo::Vector3(wx, wy, min_z));
                         mesh.addPoint(geo::Vector3(wx, wy, max_z));
                     }
+
+                    testpolys.push_back(poly);
 
                     // Calculate side triangles
                     for(int i = 0; i < num_points; ++i)
@@ -231,11 +241,47 @@ geo::ShapePtr getHeightMapShape(const tue::filesystem::Path& path, tue::config::
                         mesh.addTriangle(i * 2 + 1, j * 2, j * 2 + 1);
                     }
 
-                    TPPLPartition pp;
-                    std::list<TPPLPoly> testpolys, result;
-                    testpolys.push_back(poly);
+                    for(unsigned int i = 0; i < line_starts.size(); ++i)
+                    {
+                        int x2 = line_starts[i].x;
+                        int y2 = line_starts[i].y;
 
-                    if (!pp.Triangulate_EC(&poly, &result))
+                        while(image.at<unsigned char>(y2, x2) == v)
+                            ++x2;
+
+                        if (contour_map.at<unsigned char>(y2, x2 - 1) == 0)
+                        {
+                            // found a hole, so find the contours of this hole
+                            std::vector<geo::Vec2i> hole_points;
+                            findContours(image, geo::Vec2i(x2 - 1, y2 + 1), 1, hole_points, line_starts, contour_map, false);
+
+                            if (hole_points.size() > 2)
+                            {
+                                TPPLPoly poly_hole;
+                                poly_hole.Init(hole_points.size());
+                                poly_hole.SetHole(true);
+
+                                for(unsigned int j = 0; j < hole_points.size(); ++j)
+                                {
+                                    poly_hole[j].x = hole_points[j].x;
+                                    poly_hole[j].y = hole_points[j].y;
+
+                                    // Convert to world coordinates
+                                    double wx = hole_points[j].x * resolution + origin_x;
+                                    double wy = (image.rows - hole_points[j].y - 1) * resolution + origin_y;
+
+                                    vertex_index_map.at<int>(hole_points[j].y, hole_points[j].x) = mesh.addPoint(geo::Vector3(wx, wy, min_z));
+                                    mesh.addPoint(geo::Vector3(wx, wy, max_z));
+                                }
+                                testpolys.push_back(poly_hole);
+                            }
+                        }
+                    }
+
+                    TPPLPartition pp;
+                    std::list<TPPLPoly> result;
+
+                    if (!pp.Triangulate_EC(&testpolys, &result))
                     {
                         std::cout << "ed::models::getHeightMapShape() : ERROR: could not triangulate polygon." << std::endl;
                         return shape;
@@ -263,8 +309,8 @@ geo::ShapePtr getHeightMapShape(const tue::filesystem::Path& path, tue::config::
         }
     }
 
-//    std::cout << shape->getMesh().getPoints().size() << " vertices" << std::endl;
-//    std::cout << shape->getMesh().getTriangleIs().size() << " triangles" << std::endl;
+    std::cout << shape->getMesh().getPoints().size() << " vertices" << std::endl;
+    std::cout << shape->getMesh().getTriangleIs().size() << " triangles" << std::endl;
 
     return shape;
 }
