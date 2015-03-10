3616ac8ed3f1ae2f4dcd8fd948c949bfbb644c2c
Rokus Ottervanger <rokusottervanger@gmail.com>
2015-03-11 00:00:24 +0100

    measurement has no convex hull, entity does not automatically update convex hull when a measurement is added

--- include/ed/measurement.h
+++ include/ed/measurement.h
@@ -4,7 +4,6 @@
 #include "ed/types.h"
 #include "ed/mask.h"
 #include "ed/rgbd_data.h"
-#include "ed/convex_hull_2d.h"
 
 namespace ed
 {
@@ -18,13 +17,12 @@ public:
 
     Measurement(rgbd::ImageConstPtr image, const ImageMask& image_mask);
 
-    Measurement(const RGBDData& rgbd_data, const PointCloudMaskPtr& mask, const ConvexHull2D& convex_hull = ConvexHull2D(), unsigned int seq = 0);
+    Measurement(const RGBDData& rgbd_data, const PointCloudMaskPtr& mask, unsigned int seq = 0);
 
     const geo::Pose3D& sensorPose() const { return rgbd_data_.sensor_pose; }
     rgbd::ImageConstPtr image() const { return rgbd_data_.image; }
     PointCloudMaskConstPtr mask() const { return mask_; }
     const ImageMask& imageMask() const { return image_mask_; }
-    const ConvexHull2D& convexHull() const { return convex_hull_;  }
     double timestamp() const { return timestamp_; }
 
 protected:
@@ -34,8 +32,6 @@ protected:
     ImageMask image_mask_;
     double timestamp_;
     unsigned int seq_;
-
-    ConvexHull2D convex_hull_; // This thingy is in map frame
 
 };
 
--- src/measurement.cpp
+++ src/measurement.cpp
@@ -24,16 +24,11 @@ Measurement::Measurement(rgbd::ImageConstPtr image, const ImageMask& image_mask)
 
 // ----------------------------------------------------------------------------------------------------
 
-Measurement::Measurement(const RGBDData& rgbd_data, const PointCloudMaskPtr& mask, const ConvexHull2D& convex_hull, unsigned int seq) :
+Measurement::Measurement(const RGBDData& rgbd_data, const PointCloudMaskPtr& mask, unsigned int seq) :
     rgbd_data_(rgbd_data),
     mask_(mask),
-    timestamp_(rgbd_data.image->getTimestamp()),
-    convex_hull_(convex_hull)
+    timestamp_(rgbd_data.image->getTimestamp())
 {
-    // Calculate if not set
-    if ( convex_hull_.chull.size() == 0 )
-        helpers::ddp::get2DConvexHull(rgbd_data.point_cloud, *mask,rgbd_data.sensor_pose, convex_hull_);
-
     // Calculate image mask
     image_mask_.setSize(rgbd_data.image->getDepthImage().cols, rgbd_data.image->getDepthImage().rows);
     for(PointCloudMask::const_iterator it = mask_->begin(); it != mask_->end(); ++it)
