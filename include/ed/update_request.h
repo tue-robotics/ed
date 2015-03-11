1f1b11f51ecb186e1f5d08596758a8c2ec6c7bc9
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-03-11 11:11:30 +0100

    Fix small bug in update request

--- include/ed/update_request.h
+++ include/ed/update_request.h
@@ -20,45 +20,52 @@ class UpdateRequest
 
 public:
 
+    UpdateRequest() : empty_(true) {}
+
     // MEASUREMENTS
 
     std::map<UUID, std::vector<MeasurementConstPtr> > measurements;
-    void addMeasurement(const UUID& id, const MeasurementConstPtr& m) { measurements[id].push_back(m); }
+    void addMeasurement(const UUID& id, const MeasurementConstPtr& m) { measurements[id].push_back(m); empty_ = false; }
 
     void addMeasurements(const UUID& id, const std::vector<MeasurementConstPtr>& measurements_)
     {
+        if (measurements_.empty())
+            return;
+
         std::vector<MeasurementConstPtr>& v = measurements[id];
         v.insert(v.end(), measurements_.begin(), measurements_.end());
+
+        empty_ = false;
     }
 
 
     // SHAPES
 
     std::map<UUID, geo::ShapeConstPtr> shapes;
-    void setShape(const UUID& id, const geo::ShapeConstPtr& shape) { shapes[id] = shape; }
+    void setShape(const UUID& id, const geo::ShapeConstPtr& shape) { shapes[id] = shape; empty_ = false; }
 
 
     // CONVEX HULLS
 
     std::map<UUID, ed::ConvexHull2D> convex_hulls;
-    void setConvexHull(const UUID& id, const ed::ConvexHull2D& convex_hull) { convex_hulls[id] = convex_hull; }
+    void setConvexHull(const UUID& id, const ed::ConvexHull2D& convex_hull) { convex_hulls[id] = convex_hull; empty_ = false; }
 
     // TYPES
 
     std::map<UUID, std::string> types;
-    void setType(const UUID& id, const std::string& type) { types[id] = type; }
+    void setType(const UUID& id, const std::string& type) { types[id] = type; empty_ = false; }
 
 
     // POSES
 
     std::map<UUID, geo::Pose3D> poses;
-    void setPose(const UUID& id, const geo::Pose3D& pose) { poses[id] = pose; }
+    void setPose(const UUID& id, const geo::Pose3D& pose) { poses[id] = pose; empty_ = false; }
 
 
     // RELATIONS
 
     std::map<UUID, std::map<UUID, RelationConstPtr> > relations;
-    void setRelation(const UUID& id1, const UUID& id2, const RelationConstPtr& r) { relations[id1][id2] = r; }
+    void setRelation(const UUID& id1, const UUID& id2, const RelationConstPtr& r) { relations[id1][id2] = r; empty_ = false; }
 
 
     // DATA
@@ -80,6 +87,8 @@ public:
 
             it->second = data_total;
         }
+
+        empty_ = false;
     }
 
 
@@ -87,20 +96,16 @@ public:
 
     std::set<UUID> removed_entities;
 
-    void removeEntity(const UUID& id) { removed_entities.insert(id); }
+    void removeEntity(const UUID& id) { removed_entities.insert(id); empty_ = false; }
 
 
 
-    bool empty() const
-    {
-        return measurements.empty() &&
-               shapes.empty() &&
-               types.empty() &&
-               poses.empty() &&
-               relations.empty() &&
-               removed_entities.empty() &&
-               datas.empty();
-    }
+    bool empty() const { return empty_; }
+
+
+private:
+
+    bool empty_;
 
 };
 
