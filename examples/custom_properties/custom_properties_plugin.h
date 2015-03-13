d13054b96f0f26a2d36879eb743933e41bbe5e66
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-03-13 16:19:15 +0100

    Added another example

---
+++ examples/custom_properties/custom_properties_plugin.h
@@ -0,0 +1,35 @@
+#ifndef ED_EXAMPLES_CUSTOM_PROPERTIES_PLUGIN_H_
+#define ED_EXAMPLES_CUSTOM_PROPERTIES_PLUGIN_H_
+
+#include <ed/plugin.h>
+
+#include <geolib/datatypes.h>
+
+class CustomProperties : public ed::Plugin
+{
+
+public:
+
+    CustomProperties();
+
+    virtual ~CustomProperties();
+
+    void initialize(ed::InitData& init);
+
+    void process(const ed::WorldModel& world, ed::UpdateRequest& req);
+
+private:
+
+    // Create a key for each entity property you want to acces. This key
+    // will be used to access that property. Note that you should specify
+    // the type of the property using templates
+
+    // 'Pose' property key
+    ed::PropertyKey<geo::Pose3D> k_pose_;    
+
+    // 'Counter' property key
+    ed::PropertyKey<int> k_counter_;
+
+};
+
+#endif
