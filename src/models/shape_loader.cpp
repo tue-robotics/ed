fad2fcb3945e354fecf5a4b660ba7909dfa4f0fa
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-01-23 22:48:04 +0100

    finalize not reading paths reverse

--- src/models/shape_loader.cpp
+++ src/models/shape_loader.cpp
@@ -120,7 +120,7 @@ static std::string getUriPath(std::string type)
     else
         type_paths = &file_paths;
 
-    for(std::vector<std::string>::const_iterator it = type_paths->begin(); it != type_paths->end(); ++it)
+    for(std::vector<std::string>::const_iterator it = type_paths->cbegin(); it != type_paths->cend(); ++it)
     {
         tue::filesystem::Path file_path(*it + "/" + parsed_uri);
         if (file_path.exists())
