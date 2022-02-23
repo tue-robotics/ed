0ab67127582ebf3b9c3d326593dc2ad7dfb48815
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-02-23 10:49:08 +0100

    Longer backtrace in case of crash

--- src/ed.cpp
+++ src/ed.cpp
@@ -546,11 +546,11 @@ void signalHandler( int sig )
     std::cerr << "--------------------------------------------------" << std::endl;
     std::cerr << "Backtrace: " << std::endl << std::endl;
 
-    void *array[10];
+    void *array[20];
     size_t size;
 
     // get void*'s for all entries on the stack
-    size = backtrace(array, 10);
+    size = backtrace(array, 20);
 
     // print out all the frames to stderr
     backtrace_symbols_fd(array, size, STDERR_FILENO);
