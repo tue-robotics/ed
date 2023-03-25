9659f22a1d4fc18559cfae9a16a95e3841d704d7
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-03-25 23:05:51 +0100

    (hello_world) include logging

--- examples/hello_world/hello_world_plugin.cpp
+++ examples/hello_world/hello_world_plugin.cpp
@@ -1,5 +1,7 @@
 #include "hello_world_plugin.h"
 
+#include <ed/logging.h>
+
 #include <iostream>
 
 // ----------------------------------------------------------------------------------------------------
@@ -26,6 +28,10 @@ void HelloWorld::initialize(ed::InitData& init)
 void HelloWorld::process(const ed::WorldModel& /*world*/, ed::UpdateRequest& /*req*/)
 {
     std::cout << text_ << std::endl;
+
+    ed::log::info(text_);
+    ed::log::warning(text_);
+    ed::log::error(text_);
 }
 
 ED_REGISTER_PLUGIN(HelloWorld)
