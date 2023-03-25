0641bf8c2285aa0072985cf138d495ba20befb50
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-03-25 23:02:13 +0100

    Extend logging to support string

--- include/ed/logging.h
+++ include/ed/logging.h
@@ -2,6 +2,7 @@
 #define ED_LOGGING_H_
 
 #include <ostream>
+#include <string>
 
 namespace ed
 {
@@ -13,13 +14,19 @@ std::ostream& info();
 
 void info(const char* str);
 
+void info(const std::string& str);
+
 std::ostream& warning();
 
 void warning(const char* str);
 
+void warning(const std::string& str);
+
 std::ostream& error();
 
 void error(const char* str);
+
+void error(const std::string& str);
 
 }
 
